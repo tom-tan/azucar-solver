b85edc147c23d47f004a6e7a01aba9061fc3305b
Tomoya Tanjo <tanjo@stu.kobe-u.ac.jp>
2011-07-18 11:11:57 +0900

    z op x+y ???????????

--- src/jp/ac/kobe_u/cs/sugar/encoder/coe/OpAdd.java
+++ src/jp/ac/kobe_u/cs/sugar/encoder/coe/OpAdd.java
@@ -50,6 +50,18 @@ public class OpAdd extends RCSPLiteral {
 			csp.add(c[i]);
 		}
 
+		LLExpression[] lhs = new LLExpression[m];
+		for (int i=0; i<m-1; i++) {
+			lhs[i] = z.nth(i).add(lle(c[i+1]).mul(b));
+		}
+		lhs[m-1] = z.nth(m-1);
+
+		LLExpression[] rhs = new LLExpression[m];
+		rhs[0] = x.nth(0).add(y.nth(0));
+		for (int i=1; i<m; i++) {
+			rhs[i] = x.nth(i).add(y.nth(i)).add(lle(c[i]));
+		}
+
 		switch(op) {
 		case LE:{
 			BooleanVariable[] s = new BooleanVariable[m-1];
@@ -61,58 +73,36 @@ public class OpAdd extends RCSPLiteral {
 			// -s(i+1) or (z(i)+B*c(i+1) <= x(i)+y(i)+c(i)) (when 0 <= i < m-1)
 			for (int i=0; i<m-1; i++) {
 				Clause cls = new Clause(new BooleanLiteral(s[i+1], true));
-				LLExpression lhs = z.nth(i).add(lle(c[i+1]).mul(b));
-				LLExpression rhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
-				cls.add(lhs.le(rhs));
+				cls.add(lhs[i].le(rhs[i]));
 				ret.add(cls);
 			}
 			// z(i) <= x(i)+y(i)+c(i) (when i == m-1)
-			{
-				int i = m-1;
-				LLExpression lhs = z.nth(i);
-				LLExpression rhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
-				Clause cls = new Clause(lhs.le(rhs));
-				ret.add(cls);
-			}
+			ret.add(new Clause(lhs[m-1].le(rhs[m-1])));
 
 			// -s(i+1) or (z(i)+B*c(i+1) <= x(i)+y(i)+c(i)-1) or s(i)
 			// (when 1 <= i < m-1)
 			for (int i=1; i<m-1; i++) {
 				Clause cls = new Clause(new BooleanLiteral(s[i+1], true));
-				LLExpression lhs = z.nth(i).add(lle(c[i+1]).mul(b));
-				LLExpression rhs = x.nth(i).add(y.nth(i)).add(lle(c[i])).sub(1);
-				cls.add(lhs.le(rhs));
+				cls.add(lhs[i].le(rhs[i].sub(1)));
 				cls.add(new BooleanLiteral(s[i], false));
 				ret.add(cls);
 			}
 			// (z(i) <= x(i)+y(i)+c(i)-1) or s(i) (when i == m-1)
 			{
-				int i = m-1;
-				LLExpression lhs = z.nth(i);
-				LLExpression rhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)).sub(1) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i])).sub(1);
-				Clause cls = new Clause(lhs.le(rhs));
-				cls.add(new BooleanLiteral(s[i], false));
+				Clause cls = new Clause(lhs[m-1].le(rhs[m-1].sub(1)));
+				cls.add(new BooleanLiteral(s[m-1], false));
 				ret.add(cls);
 			}
 
 			for (int i=0; i<m-1; i++) {
-				LLExpression lhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
 				// c(i+1) <= 0 or x(i)+y(i)+c(i) >= B
 				Clause ltor = new Clause(lle(c[i+1]).le(0));
-				ltor.add(lhs.ge(b));
+				ltor.add(lhs[i].ge(b));
 				ret.add(ltor);
 
 				// c(i+1) >= 1 or x(i)+y(i)+c(i) <= B-1
 				Clause rtol = new Clause(lle(c[i+1]).ge(1));
-				rtol.add(lhs.le(b-1));
+				rtol.add(lhs[i].le(b-1));
 				ret.add(rtol);
 			}
 			return ret;
@@ -128,78 +118,47 @@ public class OpAdd extends RCSPLiteral {
 			// -s(i+1) or (z(i)+B*c(i+1) >= x(i)+y(i)+c(i)) (when 0 <= i < m-1)
 			for (int i=0; i<m-1; i++) {
 				Clause cls = new Clause(new BooleanLiteral(s[i+1], true));
-				LLExpression lhs = z.nth(i).add(lle(c[i+1]).mul(b));
-				LLExpression rhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
-				cls.add(lhs.ge(rhs));
+				cls.add(lhs[i].ge(rhs[i]));
 				ret.add(cls);
 			}
 			// z(i) >= x(i)+y(i)+c(i) (when i == m-1)
-			{
-				int i = m-1;
-				LLExpression lhs = z.nth(i);
-				LLExpression rhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
-				Clause cls = new Clause(lhs.ge(rhs));
-				ret.add(cls);
-			}
+			ret.add(new Clause(lhs[m-1].ge(rhs[m-1])));
 
 			// -s(i+1) or (z(i)+B*c(i+1)-1 >= x(i)+y(i)+c(i)) or s(i)
 			// (when 1 <= i < m-1)
 			for (int i=1; i<m-1; i++) {
 				Clause cls = new Clause(new BooleanLiteral(s[i+1], true));
-				LLExpression lhs = z.nth(i).add(lle(c[i+1]).mul(b)).sub(1);
-				LLExpression rhs = x.nth(i).add(y.nth(i)).add(lle(c[i]));
-				cls.add(lhs.ge(rhs));
+				cls.add(lhs[i].sub(1).ge(rhs[i]));
 				cls.add(new BooleanLiteral(s[i], false));
 				ret.add(cls);
 			}
 			// (z(i)-1 >= x(i)+y(i)+c(i)) or s(i) (when i == m-1)
 			{
-				int i = m-1;
-				LLExpression lhs = z.nth(i).sub(1);
-				LLExpression rhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
-				Clause cls = new Clause(lhs.ge(rhs));
-				cls.add(new BooleanLiteral(s[i], false));
+				Clause cls = new Clause(lhs[m-1].sub(1).ge(rhs[m-1]));
+				cls.add(new BooleanLiteral(s[m-1], false));
 				ret.add(cls);
 			}
 
 			for (int i=0; i<m-1; i++) {
-				LLExpression lhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
 				// c(i+1) <= 0 or x(i)+y(i)+c(i) >= B
 				Clause ltor = new Clause(lle(c[i+1]).le(0));
-				ltor.add(lhs.ge(b));
+				ltor.add(lhs[i].ge(b));
 				ret.add(ltor);
 
 				// c(i+1) >= 1 or x(i)+y(i)+c(i) <= B-1
 				Clause rtol = new Clause(lle(c[i+1]).ge(1));
-				rtol.add(lhs.le(b-1));
+				rtol.add(lhs[i].le(b-1));
 				ret.add(rtol);
 			}
 			return ret;
 		}
 
-		case EQ:{
+		case EQ:
 			for (int i=0; i<m; i++) {
-				LLExpression lhs = (i == m-1) ?
-													 z.nth(i) :
-													 z.nth(i).add(lle(c[i+1]).mul(b));
-
-				LLExpression rhs =  (i == 0) ?
-														x.nth(i).add(y.nth(i)) :
-														x.nth(i).add(y.nth(i)).add(lle(c[i]));
-
-				ret.add(new Clause(lhs.le(rhs)));
-				ret.add(new Clause(lhs.ge(rhs)));
+				ret.add(new Clause(lhs[i].le(rhs[i])));
+				ret.add(new Clause(lhs[i].ge(rhs[i])));
 			}
 			return ret;
-		}
 
 		case NE:{
 			BooleanVariable[] p = new BooleanVariable[m];
@@ -214,35 +173,24 @@ public class OpAdd extends RCSPLiteral {
 			ret.add(at_least_one);
 
 			for (int i=0; i<m; i++) {
-				LLExpression lhs = (i == m-1) ?
-													 z.nth(i) :
-													 z.nth(i).add(lle(c[i+1]).mul(b));
-
-				LLExpression rhs =  (i == 0) ?
-														x.nth(i).add(y.nth(i)) :
-														x.nth(i).add(y.nth(i)).add(lle(c[i]));
-
 				Clause cls0 = new Clause(new BooleanLiteral(p[i], true));
-				cls0.add(lhs.le(rhs.sub(1)));
+				cls0.add(lhs[i].le(rhs[i].sub(1)));
 				ret.add(cls0);
 
 				Clause cls1 = new Clause(new BooleanLiteral(q[i], true));
-				cls1.add(lhs.sub(1).ge(rhs));
+				cls1.add(lhs[i].sub(1).ge(rhs[i]));
 				ret.add(cls1);
 			}
 
 			for (int i=0; i<m-1; i++) {
-				LLExpression lhs = (i == 0) ?
-													 x.nth(i).add(y.nth(i)) :
-													 x.nth(i).add(y.nth(i)).add(lle(c[i]));
 				// c(i+1) <= 0 or x(i)+y(i)+c(i) >= B
 				Clause ltor = new Clause(lle(c[i+1]).le(0));
-				ltor.add(lhs.ge(b));
+				ltor.add(lhs[i].ge(b));
 				ret.add(ltor);
 
 				// c(i+1) >= 1 or x(i)+y(i)+c(i) <= B-1
 				Clause rtol = new Clause(lle(c[i+1]).ge(1));
-				rtol.add(lhs.le(b-1));
+				rtol.add(lhs[i].le(b-1));
 				ret.add(rtol);
 			}
 			return ret;
